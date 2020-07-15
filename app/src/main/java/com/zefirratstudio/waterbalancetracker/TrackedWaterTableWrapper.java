23ff553b1cc461fbd8fac59dbc3f7b66297eef58
Zefirrat <leonidpavlov314@gmail.com>
2020-07-15 22:30:22 +0500

    Initial commit with database connection

---
+++ app/src/main/java/com/zefirratstudio/waterbalancetracker/TrackedWaterTableWrapper.java
@@ -0,0 +1,8 @@
+package com.zefirratstudio.waterbalancetracker;
+
+public class TrackedWaterTableWrapper {
+    public static final String TableName = "TrackedWater";
+    public static final String ID = "ID";
+    public static final String DrinkDateTime = "DrinkDateTime";
+    public static final String AmountOfWater  = "AmountOfWater";
+}
